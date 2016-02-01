6853d22a3c6577bc3b2e0f8726382b7095ea4f8b
am-shopgo <ammar@shopgo.me>
2016-02-01 11:24:55 +0200

    Do some minor fixes

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'ShopGo_Limiter',
+    'ShopGo_Notifier',
     __DIR__
 );
